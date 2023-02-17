67b0cf7c9728b35928bd79b8e4db305962fc8866
harsh3468 <harshkumargoel.3468@gmail.com>
2023-02-17 14:52:34 +0530

    services has been changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import axios from "axios";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import BookingDetail from "./components/BookingDetail";
 import BookingForm from "./components/BookingForm";
-import Home from "./components/Home";
+axios.defaults.baseURL = "http://localhost:8080"
 function App() {
   return (
     <div className="body__container">
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route path="booking/form" element={<BookingForm />} />
+            <Route path="/booking/form" element={<BookingForm />} />
           </Route>
           <Route path="/booking/detail" element={<BookingDetail />} />
         </Routes>
